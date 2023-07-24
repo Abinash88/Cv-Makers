663541441e59fa1bf6e84fb5cfabf85c9d693c20
Abinash <subediabinas@gmail.com>
2023-07-24 08:50:11 +0100

    managing the errors

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import { FetchGetUser } from '@/ReduxSlices/GetUserSlice';
+import Link from 'next/link'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+const Header = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(FetchGetUser())
+  },[]) 
+
+  return (
+    <div className='w-screen flex justify-between px-5 items-center h-[70px]'>
+        <div className="headerlogo">
+            <Link className='logo text-[20px] md:text-[27px]' href={'/'}> <span>CV</span>-Maker</Link>
+        </div>
+        <div className='mr-0 md:mr-[96px]'>
+          <h5 className='text-red-600  border-2 text-[16px] md:text-[22px] rounded-full p-2 ongointtag border-red-500'> On Going Porject </h5>
+        </div>
+        <div className="">
+            <Link className='loginlinks' href={'/LoginPage'}>Login</Link>
+        </div>
+    </div>
+  )
+}
+
+export default Header
