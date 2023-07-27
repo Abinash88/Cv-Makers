a01d46d9407b44422de119a99dc0de570b12b5c0
Abinash <subediabinas@gmail.com>
2023-07-27 09:03:59 +0100

    updating the code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
 
  const LogoutBox = async() => {
   try {
-    const res = await fetch("http://localhost:3000/api/auth/Logout", {
+    const res = await fetch("/api/auth/Logout", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
